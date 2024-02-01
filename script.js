dd991e23333261a59f0d6541e295ae4f1dd5ae7f
uygar-blgn <uygarblgn13@outlook.com>
2024-02-01 19:46:53 +0300

    done

--- script.js
+++ script.js
@@ -1,75 +1,175 @@
 const gameBoard = (() => {
-    const gameArray = [['', '', ''],
+    // variables
+    let gameArray = [['', '', ''],
                        ['', '', ''],
                        ['', '', '']]
+
+
+    // functions
     const playX = (pos) => {
         gameArray[pos[0]][pos[1]] = 'X'
     }
+
     const playO = (pos) => {
         gameArray[pos[0]][pos[1]] = 'O'
     }
+
     const clearBoard = () => {
         gameArray = [['', '', ''],
                      ['', '', ''],
                      ['', '', '']]
     }
-    const hasWinner = () => {
-        if(gameArray[0][0] == gameArray[0][1] == gameArray[0][2] != '') {
-            return true
+
+    const winner = () => {
+        if(gameArray[0][0] == gameArray[0][1] && gameArray[0][1] == gameArray[0][2] && gameArray[0][2] != '') {
+            if(gameArray[0][2] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[0][2] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[1][0] == gameArray[1][1] == gameArray[1][2] != '') {
-            return true
+
+        if(gameArray[1][0] == gameArray[1][1] && gameArray[1][1] == gameArray[1][2] && gameArray[1][2] != '') {
+            if(gameArray[1][2] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[1][2] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[2][0] == gameArray[2][1] == gameArray[2][2] != '') {
-            return true
+
+        if(gameArray[2][0] == gameArray[2][1] && gameArray[2][1] == gameArray[2][2] && gameArray[2][2] != '') {
+            if(gameArray[2][2] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][2] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[0][0] == gameArray[1][0] == gameArray[2][0] != '') {
-            return true
+
+        if(gameArray[0][0] == gameArray[1][0] && gameArray[1][0] == gameArray[2][0] && gameArray[2][0] != '') {
+            if(gameArray[2][0] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][0] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[0][1] == gameArray[1][1] == gameArray[2][1] != '') {
-            return true
+
+        if(gameArray[0][1] == gameArray[1][1] && gameArray[1][1] == gameArray[2][1] && gameArray[2][1] != '') {
+            if(gameArray[2][1] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][1] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[0][2] == gameArray[1][2] == gameArray[2][2] != '') {
-            return true
+
+        if(gameArray[0][2] == gameArray[1][2] && gameArray[1][2] == gameArray[2][2] && gameArray[2][2] != '') {
+            if(gameArray[2][2] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][2] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[0][0] == gameArray[1][1] == gameArray[2][2] != '') {
-            return true
+
+        if(gameArray[0][0] == gameArray[1][1] && gameArray[1][1] == gameArray[2][2] && gameArray[2][2] != '') {
+            if(gameArray[2][2] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][2] == 'O') {
+                return 'O'
+            }
         }
-        if(gameArray[0][2] == gameArray[1][1] == gameArray[2][0] != '') {
-            return true
+
+        if(gameArray[0][2] == gameArray[1][1] && gameArray[1][1] == gameArray[2][0] && gameArray[2][0] != '') {
+            if(gameArray[2][0] == 'X') {
+                return 'X'
+            }
+            else if(gameArray[2][0] == 'O') {
+                return 'O'
+            }
         }
+
+        return false
     }
-    return {gameArray, playX, playO, clearBoard}
+
+    return {gameArray, playX, playO, clearBoard, winner}
 })()
 
 const boardInterface = (() => {
+    // variables
+    let round = 'X'
+
+    let end = ''
+
+
+    // functions
     const paintBoard = (gameBoard) => {
         for(let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
                 const pos = '#p' + i + j
+
                 const data = document.querySelector(pos)
+
                 data.textContent = gameBoard.gameArray[i][j]
             }
         }
     }
-    let round = 'X'
+
     const datas = document.querySelectorAll('td')
+
     datas.forEach((data) => {
         data.addEventListener('click', () => {
-            if(gameBoard.hasWinner) {
-                
-            }
-            else if(round == 'X' && data.textContent == '') {
+            if(round == 'X' && data.textContent == '' && !end) {
                 data.textContent = 'X'
+
                 round = 'O'
-                gameBoard.playX([data.id[2],data.id[3]])
+
+                gameBoard.playX([data.id[1],data.id[2]])
             }
-            else if(round == 'O' && data.textContent == '') {
+            else if(round == 'O' && data.textContent == '' && !end) {
                 data.textContent = 'O'
+
                 round = 'X'
-                gameBoard.playO([data.id[2],data.id[3]])
+
+                gameBoard.playO([data.id[1],data.id[2]])
+            }
+
+            if(gameBoard.winner() && !end) {
+                end = gameBoard.winner()
+            }
+
+            const message = document.querySelector('#message')
+
+            if(end != '') {
+                message.textContent = end + ' has won!'
+            }
+            else {
+                message.textContent = round + '\'s turn'
             }
         })
     })
+
+    const restart = document.querySelector('#restart')
+
+    restart.addEventListener('click', () => {
+        round = 'X'
+
+        end = ''
+
+        gameBoard.clearBoard()
+
+        datas.forEach((data) => {
+            data.textContent = ''
+        })
+
+        const message = document.querySelector('#message')
+
+        message.textContent = 'X\'s turn'
+    })
+
     return {paintBoard, round}
 })()
